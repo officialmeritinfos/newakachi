9807a600e1a367a43156035a77005f8b05e6f328
Anon Dev <michaelerastus9@gmail.com>
2024-04-18 08:43:34 +0100

    Completed

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -450,6 +450,9 @@
         </div>
     </div>
 
+
+
+
     <div style="background-color: #01123c;" class="uk-section uk-section-secondary uk-padding-large uk-background-contain uk-background-bottom-center in-padding-large-vertical@s in-profit-3" data-src="{{asset('home/img/in-section-profit-3.png')}}" data-uk-img>
         <div class="uk-container uk-margin-small-bottom">
             <div class="uk-grid-large" data-uk-grid>
@@ -513,7 +516,7 @@
                         ABOUT FIXED FUNDS DEPOSIT
                     </h2>
                     <div class="m-b-40 f15">
-                        <p style="color: #fff !important">Fixed Deposit (FD) Is an organized investment package that allows you as an investor to manage your Bitcoin account with a higher interest rate other than the ordinary investment packages,this involves depositing and making withdrawals ‘monthly or yearly’.</p>
+                        <p style="color: #fff !important">Fixed Deposit (FD) Is an organized investment package that allows you as an investor to manage your Investment Portfolio with a higher interest rate other than the ordinary investment packages,this involves depositing and making withdrawals ‘monthly or yearly’.</p>
                         <p style="color: #fff !important"> In this package, you either do the fixed deposit ‘starter plan or standard plan’ and having in mind that no withdrawals are to be made until the agreed monthly or yearly date. No separate account is needed in this package, you can start with a direct fixed deposit account or migrate from your ordinary account to it, and no fees are to be paid to do this. In a fixed investment, investors are opportune to get a 2.5% weekly interest for the starter plan and 3% weekly interest rate for the standard plan.. This also means that the stater plan minimum deposit is with the range of $50,000 to $99,999(maximum).
                             And standard to be (minimum) $100,000 to unlimited.</p>
                         <p style="color: #fff !important"> You are also eligible to request for a premature withdrawal from your fixed deposit funds in case of any emergency or sudden financial situations. However,as a certain amount of penalty will be paid to achieve this,
@@ -827,6 +830,45 @@
 {{--            </div>--}}
 {{--        </div>--}}
 {{--    </section>--}}
+
+    <section class="pftb theme-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12" style="padding-bottom: 30px;">
+                    <div class="uk-text-center">
+                        <div class="in-icon-wrapper circle uk-margin-remove-bottom">
+                        <span data-uk-icon="icon:fa-medal; ratio: 0.054" class="uk-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="27.648" height="27.648" data-svg="fa-medal">
+                                <path d="M223.75 130.75L154.62 15.54A31.997 31.997 0 0 0 127.18 0H16.03C3.08 0-4.5 14.57 2.92 25.18l111.27 158.96c29.72-27.77 67.52-46.83 109.56-53.39zM495.97 0H384.82c-11.24 0-21.66 5.9-27.44 15.54l-69.13 115.21c42.04 6.56 79.84 25.62 109.56 53.38L509.08 25.18C516.5 14.57 508.92 0 495.97 0zM256 160c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm92.52 157.26l-37.93 36.96 8.97 52.22c1.6 9.36-8.26 16.51-16.65 12.09L256 393.88l-46.9 24.65c-8.4 4.45-18.25-2.74-16.65-12.09l8.97-52.22-37.93-36.96c-6.82-6.64-3.05-18.23 6.35-19.59l52.43-7.64 23.43-47.52c2.11-4.28 6.19-6.39 10.28-6.39 4.11 0 8.22 2.14 10.33 6.39l23.43 47.52 52.43 7.64c9.4 1.36 13.17 12.95 6.35 19.59z">
+                                </path>
+                            </svg></span>
+                        </div>
+                    </div>
+                    <div class="heading-text heading-line text-center">
+                        <p data-animate="zoomIn" style="color: #fff !important;">{{$siteName}}</p>
+                    </div>
+
+                    <h2 style="font-size: 23px; margin-bottom: 0; color: #fff !important;" class="tup text-center">
+                        HOW WE GENERATE MONEY
+                    </h2>
+                </div>
+                @foreach($services as $service)
+                    <div class="col-lg-4 m-b-30" data-animate="fadeInLeft">
+                        <div class="card" >
+                            <img class="card-img-top" src="{{asset('home/serv/'.$service->photo)}}" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$service->title}}</h5>
+                                <p class="card-text">
+                                    {{\Illuminate\Support\Str::words($service->short,50)}}
+                                </p>
+                                <a href="{{route('service.details',['id'=>$service->id])}}" class="btn btn-primary">Read more</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </section>
 
     <section class="pftb theme-bg">
         <div class="container">
